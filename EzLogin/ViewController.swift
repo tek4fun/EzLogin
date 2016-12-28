1dae4fe301630ea97252abe72cca98d49f9bab8f
iOS Student <iosstudent@techmasters-iMac.local>
2016-12-28 16:29:28 +0700

    update

--- EzLogin/ViewController.swift
+++ EzLogin/ViewController.swift
@@ -9,17 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var users = ["tu":"111","thanh":"222","duc":"333"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+            }
+    
+    
+    @IBOutlet weak var tf_user: UITextField!
+    @IBOutlet weak var tf_pass: UITextField!
+    @IBAction func action_Login(_ sender: AnyObject) {
+        if let password = users[tf_user.text!]{
+            if password == tf_pass.text{
+                print("Dang nhap thanh cong!")
+            }else{
+                print("Tai khoan hoac Mat khau khong chinh xac!")
+            }
+        }
+        else{
+            print("Tai khoan khong ton tai!")
+        }
     }
+    
+    @IBAction func action_List(_ sender: AnyObject) {
+        for (tentaikhoan, matkhau) in users{
+            print("ten tai khoan: ", tentaikhoan)
+            print("mat khau: ",matkhau)
+        }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func action_Add(_ sender: AnyObject) {
+        if users[tf_user.text!] != nil{
+            print("Tai khoan da ton tai!")
+        }else{
+            users[tf_user.text!] = tf_pass.text!
+            print("Them tai khoan thanh cong!")
+        }
+    }
+    
+    @IBAction func action_Delete(_ sender: AnyObject) {
+        if users[tf_user.text!] != nil{
+            users.removeValue(forKey: tf_user.text!)
+            print("Xoa thanh cong!")
+        }else{
+            print("Tai khoan khong ton tai!")
+        }
+    }
 
 }
 
